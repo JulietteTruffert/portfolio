0c96f7133c82c2596f476fdd3070917d5e21f475
Juliette TRUFFERT <juliette.truffert@gmail.com>
2025-04-17 10:18:42 +0200

    Adding animations to sections when scrolled

---
+++ scroll-animation.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll('section');
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        //   observer.unobserve(entry.target); // Optionnel : ne l'anime qu'une fois
+        } else {
+            entry.target.classList.remove('visible');
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+});
+  
